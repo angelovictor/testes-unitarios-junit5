e71038a12dadeed1bc24ead15572d233e4e3ec70
Ângelo Victor <angelovictor.ti@gmail.com>
2022-09-26 05:23:43 -0300

    inclusao do petshop testes na suite

--- src/test/java/suites/SuiteControllerClasses.java
+++ src/test/java/suites/SuiteControllerClasses.java
@@ -1,14 +1,16 @@
 package suites;
 
-import controller.ClienteControllerTest;
 import org.junit.platform.suite.api.Suite;
 import org.junit.platform.suite.api.SuiteDisplayName;
 import org.junit.platform.suite.api.SelectClasses;
+
+import controller.*;
 
 @Suite
 @SuiteDisplayName("Suite Controller Testes Classes")
 @SelectClasses({
         ClienteControllerTest.class,
+        PetShopControllerTest.class,
         //Adicionar demais classes e separar por virgula...
 })
 public class SuiteControllerClasses {
